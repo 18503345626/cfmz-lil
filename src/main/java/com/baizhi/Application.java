79cab019a03465a15a5589ab48573f524533034b
ll <981873019@qq.com>
2021-05-09 10:24:03 +0800

    20210509

--- src/main/java/com/baizhi/Application.java
+++ src/main/java/com/baizhi/Application.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.baizhi.dao")
 public class Application {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
